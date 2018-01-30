73c32eeadb5e989a5ce2f8d38f8804e8bd354156
gregwhitaker <gwhitake@gmail.com>
2018-01-30 14:56:24 -0800

    Doing work

---
+++ client/src/main/java/io/netifi/proteus/example/client/Main.java
@@ -0,0 +1,8 @@
+package io.netifi.proteus.example.client;
+
+public class Main {
+
+    public static void main(String... args) {
+
+    }
+}
---
+++ service/src/main/java/io/netifi/proteus/example/service/Main.java
@@ -0,0 +1,8 @@
+package io.netifi.proteus.example.service;
+
+public class Main {
+
+    public static void main(String... args) {
+
+    }
+}
